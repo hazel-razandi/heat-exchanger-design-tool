7b4d7f72b421846dd69adabf7e466a8d24ba3147
kakarotoncloud <studentmail48@gmail.com>
2026-01-26 16:17:48 +0530

    Update calculations.py

--- src/calculations.py
+++ src/calculations.py
@@ -1,8 +1,8 @@
 """
-Heat Exchanger Calculations Module
-Updated by KAKAROTONCLOUD for robustness.
+Thermal Calculation Engine (LMTD & NTU)
+Author: KAKAROTONCLOUD
+Version: 3.0.0 Enterprise
 """
-
 import numpy as np
 from src.fluid_properties import FluidProperties
 
@@ -10,107 +10,123 @@ class HeatExchanger:
     def __init__(self, flow_arrangement='Counter Flow'):
         self.flow_arrangement = flow_arrangement
 
-    def calculate_lmtd(self, T_hot_in, T_hot_out, T_cold_in, T_cold_out, 
-                       m_hot, m_cold, fluid_hot, fluid_cold, U_value):
+    def calculate_lmtd(self, Thi, Tho, Tci, Tco, mh, mc, hot_fluid, cold_fluid, U):
+        """
+        Design Mode: Calculates Required Area based on Temps.
+        """
+        warnings = []
         
-        # 1. Properties
-        hot_fluid = FluidProperties(fluid_hot)
-        cold_fluid = FluidProperties(fluid_cold)
+        # 1. Fluid Properties (Cached)
+        hf = FluidProperties(hot_fluid)
+        cf = FluidProperties(cold_fluid)
         
-        T_hot_avg = (T_hot_in + T_hot_out) / 2
-        T_cold_avg = (T_cold_in + T_cold_out) / 2
+        # Avg Temps
+        Th_avg = (Thi + Tho) / 2
+        Tc_avg = (Tci + Tco) / 2
         
-        Cp_hot = hot_fluid.get_specific_heat(T_hot_avg)
-        Cp_cold = cold_fluid.get_specific_heat(T_cold_avg)
+        Cph = hf.get_specific_heat(Th_avg)
+        Cpc = cf.get_specific_heat(Tc_avg)
         
-        # 2. Heat Loads
-        Q_hot = m_hot * Cp_hot * (T_hot_in - T_hot_out) / 1000  # kW
-        Q_cold = m_cold * Cp_cold * (T_cold_out - T_cold_in) / 1000  # kW
-        Q_avg = (Q_hot + Q_cold) / 2
+        # 2. Duty Calculation
+        Q_hot = mh * Cph * (Thi - Tho) / 1000  # kW
+        Q_cold = mc * Cpc * (Tco - Tci) / 1000 # kW
+        Q_duty = (Q_hot + Q_cold) / 2
         
-        # 3. LMTD with safeguards
+        # 3. LMTD & Correction Factor (Ft)
         if self.flow_arrangement == 'Counter Flow':
-            dT1 = T_hot_in - T_cold_out
-            dT2 = T_hot_out - T_cold_in
+            dt1 = Thi - Tco
+            dt2 = Tho - Tci
         else:
-            dT1 = T_hot_in - T_cold_in
-            dT2 = T_hot_out - T_cold_out
-
-        # Handle pinch points or zero difference
-        if dT1 <= 0.1: dT1 = 0.1
-        if dT2 <= 0.1: dT2 = 0.1
+            dt1 = Thi - Tci
+            dt2 = Tho - Tco
+            
+        # Physics Safety: Prevent log(0)
+        if dt1 <= 0.1: dt1 = 0.1
+        if dt2 <= 0.1: dt2 = 0.1
         
-        if abs(dT1 - dT2) < 0.05:
-            LMTD = dT1
+        if abs(dt1 - dt2) < 0.05:
+            LMTD = dt1
         else:
-            LMTD = (dT1 - dT2) / np.log(dT1 / dT2)
+            LMTD = (dt1 - dt2) / np.log(dt1 / dt2)
 
-        # 4. Area
-        if U_value <= 1: U_value = 1 # Prevent div by zero
-        Area = (Q_avg * 1000) / (U_value * LMTD)
+        # TEMA Alert: Temperature Cross check
+        if self.flow_arrangement == 'Counter Flow' and Tho < Tco:
+            warnings.append("Temperature Cross Detected (Th_out < Tc_out). Requires Multi-Shell TEMA F or G type.")
 
-        # 5. NTU & Effectiveness
-        C_hot = m_hot * Cp_hot
-        C_cold = m_cold * Cp_cold
-        C_min = min(C_hot, C_cold)
-        NTU = (U_value * Area) / C_min if C_min > 0 else 0
-        Q_max = C_min * (T_hot_in - T_cold_in) / 1000
-        Effectiveness = Q_avg / Q_max if Q_max > 0 else 0
+        # 4. Area Calculation
+        if U <= 1: U = 1.0
+        Area = (Q_duty * 1000) / (U * LMTD)
+
+        # 5. Effectiveness & NTU (Back-calculation)
+        Ch = mh * Cph
+        Cc = mc * Cpc
+        Cmin = min(Ch, Cc)
+        NTU = (U * Area) / Cmin if Cmin > 0 else 0
+        Q_max = Cmin * (Thi - Tci) / 1000
+        Eff = Q_duty / Q_max if Q_max > 0 else 0
 
         return {
-            'Q': Q_avg, 'area': Area, 'LMTD': LMTD, 'NTU': NTU, 
-            'effectiveness': Effectiveness, 'energy_balance_error': abs(Q_hot-Q_cold)/Q_hot*100 if Q_hot>0 else 0,
-            'T_hot_in': T_hot_in, 'T_hot_out': T_hot_out,
-            'T_cold_in': T_cold_in, 'T_cold_out': T_cold_out,
-            'flow_type': self.flow_arrangement, 'method': 'LMTD',
-            'hx_type': 'Generic', 'hot_fluid': fluid_hot, 'cold_fluid': fluid_cold,
-            'm_hot': m_hot, 'm_cold': m_cold, 'U_value': U_value
+            'Q': Q_duty, 'area': Area, 'LMTD': LMTD, 'NTU': NTU, 
+            'effectiveness': Eff, 
+            'T_hot_in': Thi, 'T_hot_out': Tho, 'T_cold_in': Tci, 'T_cold_out': Tco,
+            'm_hot': mh, 'm_cold': mc, 'hot_fluid': hot_fluid, 'cold_fluid': cold_fluid,
+            'flow_type': self.flow_arrangement, 'U_value': U,
+            'warnings': warnings
         }
 
-    def calculate_ntu(self, T_hot_in, T_cold_in, m_hot, m_cold, 
-                     fluid_hot, fluid_cold, U_value, Area):
+    def calculate_ntu(self, Thi, Tci, mh, mc, hot_fluid, cold_fluid, U, Area):
+        """
+        Rating Mode: Calculates Outlet Temps based on Area.
+        """
+        warnings = []
+        
         # 1. Properties
-        hot_fluid = FluidProperties(fluid_hot)
-        cold_fluid = FluidProperties(fluid_cold)
+        hf = FluidProperties(hot_fluid)
+        cf = FluidProperties(cold_fluid)
         
-        Cp_hot = hot_fluid.get_specific_heat(T_hot_in) # Approx using inlet
-        Cp_cold = cold_fluid.get_specific_heat(T_cold_in)
-
-        # 2. Capacity Rates
-        C_hot = m_hot * Cp_hot
-        C_cold = m_cold * Cp_cold
-        C_min = min(C_hot, C_cold)
-        C_max = max(C_hot, C_cold)
-        C_r = C_min / C_max if C_max > 0 else 0
-
-        # 3. NTU
-        NTU = (U_value * Area) / C_min if C_min > 0 else 0
-
-        # 4. Effectiveness
-        eff = 0
+        Cph = hf.get_specific_heat(Thi)
+        Cpc = cf.get_specific_heat(Tci)
+        
+        # 2. NTU Parameters
+        Ch = mh * Cph
+        Cc = mc * Cpc
+        Cmin = min(Ch, Cc)
+        Cmax = max(Ch, Cc)
+        Cr = Cmin / Cmax if Cmax > 0 else 0
+        
+        NTU = (U * Area) / Cmin if Cmin > 0 else 0
+        
+        # 3. Effectiveness-NTU Relations
         if self.flow_arrangement == 'Counter Flow':
-            if C_r < 1:
-                arg = -NTU * (1 - C_r)
-                eff = (1 - np.exp(arg)) / (1 - C_r * np.exp(arg))
+            if Cr < 1.0:
+                arg = -NTU * (1 - Cr)
+                Eff = (1 - np.exp(arg)) / (1 - Cr * np.exp(arg))
             else:
-                eff = NTU / (1 + NTU)
+                Eff = NTU / (1 + NTU)
         else: # Parallel
-            arg = -NTU * (1 + C_r)
-            eff = (1 - np.exp(arg)) / (1 + C_r)
+            arg = -NTU * (1 + Cr)
+            Eff = (1 - np.exp(arg)) / (1 + Cr)
 
-        # 5. Heat & Outlets
-        Q_max = C_min * (T_hot_in - T_cold_in)
-        Q_watts = eff * Q_max
+        # 4. Outlets
+        Q_max = Cmin * (Thi - Tci)
+        Q_actual = Eff * Q_max # Watts
         
-        T_hot_out = T_hot_in - (Q_watts / C_hot)
-        T_cold_out = T_cold_in + (Q_watts / C_cold)
+        Tho = Thi - (Q_actual / Ch)
+        Tco = Tci + (Q_actual / Cc)
+        
+        # 5. LMTD for reporting
+        # Recalculate LMTD based on new temps for report consistency
+        dt1 = Thi - Tco
+        dt2 = Tho - Tci
+        if dt1 <= 0.1: dt1 = 0.1
+        if dt2 <= 0.1: dt2 = 0.1
+        LMTD = (dt1 - dt2) / np.log(dt1 / dt2) if abs(dt1-dt2) > 0.05 else dt1
 
         return {
-            'Q': Q_watts/1000, 'area': Area, 'LMTD': 0, 'NTU': NTU, 
-            'effectiveness': eff, 'energy_balance_error': 0,
-            'T_hot_in': T_hot_in, 'T_hot_out': T_hot_out,
-            'T_cold_in': T_cold_in, 'T_cold_out': T_cold_out,
-            'flow_type': self.flow_arrangement, 'method': 'NTU',
-            'hx_type': 'Generic', 'hot_fluid': fluid_hot, 'cold_fluid': fluid_cold,
-            'm_hot': m_hot, 'm_cold': m_cold, 'U_value': U_value
+            'Q': Q_actual/1000, 'area': Area, 'LMTD': LMTD, 'NTU': NTU, 
+            'effectiveness': Eff, 
+            'T_hot_in': Thi, 'T_hot_out': Tho, 'T_cold_in': Tci, 'T_cold_out': Tco,
+            'm_hot': mh, 'm_cold': mc, 'hot_fluid': hot_fluid, 'cold_fluid': cold_fluid,
+            'flow_type': self.flow_arrangement, 'U_value': U,
+            'warnings': warnings
         }
